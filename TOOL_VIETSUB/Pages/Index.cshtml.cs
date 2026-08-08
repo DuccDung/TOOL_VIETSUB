a0eec76286b61fad54e942ff0ce890f2eab0a7f3
DuccDung <duccdung999@gmail.com>
2026-08-08 23:36:09 +0700

    setup project

---
+++ TOOL_VIETSUB/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TOOL_VIETSUB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
